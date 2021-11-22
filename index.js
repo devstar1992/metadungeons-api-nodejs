2178d21850e7c33fed3fbbaade5e62bfe58fcb4d
devstar1992 <75045062+devstar1992@users.noreply.github.com>
2021-11-22 08:20:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const path = require('path')
 const moment = require('moment')
 const { HOST } = require('./src/constants')
 const db = require('./src/database')
-const goldSetABI=require('./abi/GoldCraftingSet_metadata.json');
-const silverSetABI=require('./abi/SilverCraftingSet_metadata.json');
-const mithrilSetABI=require('./abi/MithrilCraftingSet_metadata.json');
+const goldSetABI = require('./abi/GoldCraftingSet_metadata.json');
+const silverSetABI = require('./abi/SilverCraftingSet_metadata.json');
+const mithrilSetABI = require('./abi/MithrilCraftingSet_metadata.json');
 // const factorySetABI=require('./abi/SetFactory_metadata.json');
-const silverAddress="";
-const goldAddress="0xB63901F32fEc8581275632E678B52BE4764208d3";
-const mithrilAddress="";
+const silverAddress = "";
+const goldAddress = "0xB63901F32fEc8581275632E678B52BE4764208d3";
+const mithrilAddress = "";
 // const factoryAddress="";
-console.log(goldSetABI);
 
 const PORT = process.env.PORT || 3001
 const Web3 = require("web3");
-const url = "wss://eth-mainnet.alchemyapi.io/v2/UdVl55H5KSJkdnZfXcn47IC_j3EhObCO";
+const url = "wss://eth-rinkeby.alchemyapi.io/v2/zT6MSYFVB-ojEc0-BbokQELJKOl0YxdS";
 
 const options = {
   timeout: 30000,
@@ -41,17 +40,17 @@ const app = express()
 // Static public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Get ready for OpenSea!');
 })
 
-app.get('/api/gold_sets/:token_id',async function(req, res) { 
+app.get('/api/gold_sets/:token_id', async function (req, res) {
   const contract = new web3.eth.Contract(goldSetABI, goldAddress);
-  try{
-  const info=await contract.methods.getTokenInfoSuffix(1).call();
-  console.log(info);
+  try {
+    const info = await contract.methods.getTokenInfoSuffix(1).call();
+    console.log(info);
 
-  }catch(err){
+  } catch (err) {
     console.log(err)
   }
   // const data = {
@@ -65,7 +64,7 @@ app.get('/api/gold_sets/:token_id',async function(req, res) {
   res.send(info)
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 })
 
